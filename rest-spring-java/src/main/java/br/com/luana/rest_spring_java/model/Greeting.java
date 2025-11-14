9833653b9810c61603c3acdd3c362f3c232a7140
luana.pereira <luana.pereira@memora.com.br>
2025-11-14 13:44:37 -0300

    First steps in java-spring

---
+++ rest-spring-java/src/main/java/br/com/luana/rest_spring_java/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.luana.rest_spring_java.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-spring-java/src/test/java/br/com/luana/rest_spring_java/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.luana.rest_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
